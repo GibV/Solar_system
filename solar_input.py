d23782d9a92211887ea66f4780f37563b4de959d
GibV <gibvize123@gmail.com>
2023-11-24 16:59:22 +0300

    Write state sin file was implemented
    states of sysstem objects save in file

--- solar_input.py
+++ solar_input.py
@@ -96,10 +96,12 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            w.write(f'{obj.type} {obj.R} {obj.color} {obj.m} {obj.x} {obj.y} {obj.Vx} {obj.Vy}')
+            out_file.write(f'{obj.type} {obj.R} {obj.color} {obj.m} {obj.x} {obj.y} {obj.Vx} {obj.Vy}\n')
             # FIXME: should store real values
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
+##def save
+
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
